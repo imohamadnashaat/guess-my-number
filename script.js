838572153693fb695c95e1f5d9a1a016a7c9d798
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-02-07 11:44:02 +0300

    add the game logic

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+    document.querySelector('.guess').style.borderColor = 'red';
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔺 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👻 Game Over!';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔻 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👻 Game Over!';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
+  }
+});
